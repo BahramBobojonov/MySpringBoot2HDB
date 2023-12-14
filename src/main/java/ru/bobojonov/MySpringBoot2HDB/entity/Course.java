3f80eae0a5cb3b5eda5c5de2c28d86d5c48e4a7b
vivar <bobojonoffff@gmail.com>
2023-12-14 15:30:13 +0300

    fix problem lab05

--- src/main/java/ru/bobojonov/MySpringBoot2HDB/entity/Course.java
+++ src/main/java/ru/bobojonov/MySpringBoot2HDB/entity/Course.java
@@ -3,7 +3,6 @@ package ru.bobojonov.MySpringBoot2HDB.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 @Setter
 @Getter
